13e07f3be064b79e2d15022af1a7c5ecd0d5b52b
Payam1991Gr <pgerackoohi@gmail.com>
2022-10-27 11:38:19 +0330

    removing HOST

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const HOST = 'localhost'
+// const HOST = 'localhost'
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (_, res) => res.redirect('index.html'))
-	.listen(PORT, HOST, () => console.log(`Listening on ${PORT}`))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`))
+	// .listen(PORT, HOST, () => console.log(`Listening on ${PORT}`))
