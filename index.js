14f064538843d5275e6a8d600abef958826af77b
Payam1991Gr <pgerackoohi@gmail.com>
2022-10-29 10:01:13 +0330

    index.js: no HOST test

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const HOST = process.env.HOST || 'localhost'
+// const HOST = process.env.HOST || 'localhost'
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.use(express.static(path.join(__dirname, 'frontend/dist')))
 	.get('/', (_, res) => res.redirect('index.html'))
-	.listen(PORT, HOST, () => console.log(`Listening on ${HOST}:${PORT}`))
+	.listen(PORT, () => console.log(`Listening on ${HOST}:${PORT}`))
+	// .listen(PORT, HOST, () => console.log(`Listening on ${HOST}:${PORT}`))
