4c4e9b8b4a4f5f23c429cd192707274ab6589832
Payam1991Gr <pgerackoohi@gmail.com>
2022-10-30 14:16:22 +0330

    PWA-Solid has issue updating sass users, when sass is changed!

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,9 @@
 import { lazy } from 'solid-js';
 import { render } from 'solid-js/web'
 import { Router, Routes, Route } from 'solid-app-router';
+
 import ReloadPrompt from './pages/modules/reload-prompt';
+
 import './index.css'
 
 const Home = lazy(() => import('./pages/home'))
